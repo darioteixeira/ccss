b0d6a0f6c125b0daa2548cc89d37d58c39910654
darioteixeira <darioteixeira@yahoo.com>
2016-01-21 14:51:58 +0000

    Update copyright headers

--- src/ccss.ml
+++ src/ccss.ml
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*  Ccss.ml
-    Copyright (c) 2010 Dario Teixeira (dario.teixeira@yahoo.com)
+    Copyright (c) 2010-2016 Dario Teixeira <dario.teixeira@nleyten.com>
     This software is distributed under the terms of the GNU GPL version 2.
     See LICENSE file for full license text.
 *)
