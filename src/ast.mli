155657a7ec7326e92a7a50440324ee6401b8aa50
darioteixeira <darioteixeira@yahoo.com>
2016-01-22 11:35:38 +0000

    Renaming: ast.ml -> ast.mli (since it contains only type definitions)

--- src/ast.mli
+++ src/ast.mli
@@ -1,5 +1,5 @@
 (********************************************************************************)
-(*  Ast.ml
+(*  Ast.mli
     Copyright (c) 2010-2016 Dario Teixeira <dario.teixeira@nleyten.com>
     This software is distributed under the terms of the GNU GPL version 2.
     See LICENSE file for full license text.
